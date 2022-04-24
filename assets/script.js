d2b5d5af896fe244e5adfec49658d8469ba14716
jinjoolee1 <jinjoolee126@gmail.com>
2022-04-24 11:29:25 -0500

    js updated

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,8 @@
 var dadJokeApiUrl = "https://icanhazdadjoke.com";
 var inspQuotesApiUrl = "https://api.aakhilv.me";
 var sunriseSunsetApiUrl ="https://api.sunrise-sunset.org/json";
-var randomJokesApiUrl ="https://api.humorapi.com/jokes/random?api-key=b00f3ac51d2e490a9253aecae0b40001";
+ var randomJokesApiUrl ="https://api.humorapi.com/jokes/random?api-key=b00f3ac51d2e490a9253aecae0b40001";
 // var randomJokesApiUrl ="https://api.humorapi.com/jokes/random";
-
 // DadJoke
 var displayDad = document.getElementById('displayJokes');
 var dadJokes = document.getElementById('dad-jokes');
@@ -22,6 +21,40 @@ async function dadJoke() {
 
 dadJokes.addEventListener('click', dadJoke);
 
+// Header & background image API
+let xmlhttp = new XMLHttpRequest ();
+xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200){
+        let data = JSON.parse(this.responseText);
+        let copyright = data['copyright'];
+        let date = data['date']
+        let explanation = data['explanation']
+        let hdurl = data['hdurl']
+        let media_type = data['media_type']
+        let title = data['title']
+        let url = data['url']
+        document.getElementById("wrapper-url").style.backgroundImage = `url("${url}")`;
+        document.getElementById("wrapper-title").innerHTML = title;
+        document.getElementById('wrapper-copyright').innerHTML = copyright
+        document.getElementById('wrapper-explanation').innerHTML = explanation
+        document.getElementById('wrapper-hdurl').href = hdurl
+    }
+}
+var about = document.getElementById('about')
+var aboutInfo = document.getElementById('aboutInfo')
+let queryUrl = "https://api.nasa.gov/planetary/apod?";
+let queryKey = "api_key=hgYbSzucAKEBVirb5pBcqp6I2boGGesw2D0GRhaZ";
+let queryFull = queryUrl + queryKey;
+xmlhttp.open('GET', queryFull, true);
+xmlhttp.send();
+about.addEventListener('click', function(){
+    if(aboutInfo.style.display === "none") {
+        aboutInfo.style.display = 'block'
+    }else{
+        aboutInfo.style.display = 'none'
+    }
+})
+
 // RandomJoke
 var displayRandom = document.getElementById('random');
 var randomJokes = document.getElementById('random-jokes');
@@ -34,7 +67,7 @@ async function jokes() {
         }
     });
     var random = await res.json();
-    var textareaRandomJokes = random.jokes;
+    var textareaRandomJokes = random.joke;
     console.log(random);
     displayRandom.innerText = textareaRandomJokes;
 }
